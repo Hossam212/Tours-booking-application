352e5b1da2cbb1fcd18165e6ac2613d2b2b16d3c
Hossam <hossam995@gmail.com>
2023-07-07 01:54:29 +0300

    Updated server.js

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
+const port = process.env.PORT || 3000;
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log('DB Connected succesfully'));
-const server = app.listen('3000', () => {
+const server = app.listen(port, () => {
   console.log('Listening for requests on port 3000....');
 });
 
